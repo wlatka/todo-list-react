cbeccf7fc56b8ce5adf140d892cf49a2d12a7e1c
Wojciech Latka <w.latka@itprotech.pl>
2021-12-07 21:19:03 +0100

    change of name to generic

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -19,7 +19,7 @@ function* saveTasksInLocalStorageHandler() {
     yield call(saveTasksInLocalStorage, tasks);
 }
 
-export function* watchFetchExampleTasks() {
+export function* tasksSaga() {
     yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
     yield takeEvery("*", saveTasksInLocalStorageHandler);
 }

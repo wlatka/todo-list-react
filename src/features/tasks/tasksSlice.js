bc83045dc40cf2aeb729943fb91b743cd5eb255c
Wojciech Latka <w.latka@itprotech.pl>
2021-12-07 20:59:10 +0100

    saving to localStorage

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -7,28 +7,40 @@ const tasksSlice = createSlice({
         hideDone: false,
     },
     reducers: {
-        addTask: ({ tasks }, { payload }) => {
-            tasks.push(payload);
+        addTask: ({ tasks }, { payload: newTask }) => {
+            tasks.push(newTask);
         },
         toggleHideDone: state => {
             state.hideDone = !state.hideDone;
         },
-        removeTask: ({ tasks }, { payload }) => {
-            const index = tasks.findIndex(({ id }) => id === payload);
+        removeTask: ({ tasks }, { payload: deleteTask }) => {
+            const index = tasks.findIndex(({ id }) => id === deleteTask);
             tasks.splice(index, 1)
         },
-        toggleTaskDone: ({ tasks }, { payload }) => {
-            const index = tasks.findIndex(({ id }) => id === payload);
+        toggleTaskDone: ({ tasks }, { payload: toggleTask }) => {
+            const index = tasks.findIndex(({ id }) => id === toggleTask);
             tasks[index].done = !tasks[index].done
         },
         setAllDone: ({ tasks }) => {
             for (const task of tasks) {
                 task.done = true;
             }
-        }
+        },
+        fetchExampleTasks: () => { },
+        setTasks: (state, { payload: tasks }) => {
+            state.tasks = tasks;
+        },
     },
 });
 
-export const { addTask, removeTask, toggleTaskDone, toggleHideDone, setAllDone } = tasksSlice.actions;
+export const {
+    addTask,
+    removeTask,
+    toggleTaskDone,
+    toggleHideDone,
+    setAllDone,
+    fetchExampleTasks,
+    setTasks
+} = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
